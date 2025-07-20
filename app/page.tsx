aaa66e9e4d1e31536231990fc901d38c55e01532
amineas <amineassid01@gmail.com>
2025-07-20 05:01:18 +0200

    Implement Navigation Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,42 @@
 import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
     <main>
-      <h1 className="text-2xl underline">Popular Companions</h1>
+      <h1 >Popular Companions</h1>
         
         
         <section className="home-section">
-                  <CompanionCard />
-                  <CompanionCard />
-                  <CompanionCard />
+            <CompanionCard 
+              id="123"
+              name="Neura the Brainy Explorer"
+              topic="Neural Networks of the Brain"
+              subject="science"
+              duration={45}
+              color="#ffda6e"
+          /><CompanionCard 
+              id="456"
+              name="Countsy the Number Wizard"
+              topic="Derivatives & integrals"
+              subject="maths"
+              duration={30}
+              color="#e5d0ff"
+          />
+            <CompanionCard 
+              id="789"
+              name="Neura the Brainy Explorer"
+              topic="languge"
+              subject="English Literature"
+              duration={45}
+              color="#BDE7FF"
+          />
+        </section>
+
+        <section className="home-section">
+          <CompanionsList />
+          <CTA />
         </section>
     </main>
     
---
+++ components/CTA.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Cta = () => {
+  return (
+    <div>Cta</div>
+  )
+}
+
+export default Cta
+
